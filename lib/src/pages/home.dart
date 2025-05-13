14332f385dd3d899c766232a03f7b036a921191b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-05-13 18:52:26 -0300

    criando_um_'modelo'_para_construir_as_telas_com_acesso_total_utilizando_'Consumer'

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/appdata.dart';
 
 class HomePage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text('Home'),
-                backgroundColor: Color(0xFF2E8BF1),
-            ),
-            body: Center(
-                child: Text('Inicio'),
-            ),
+        return Consumer<AppData>(       // 'Consumer' escuta as mudanças em `AppData` e reconstrói a tela quando o estado de `AppData` mudar
+            builder: (ctx, appdata, child) => Scaffold(     // apartir do 'Scaffold' e criada a estrutura pricipal da tela
+                appBar: AppBar(
+                    title: Text('Home'),
+                    backgroundColor: Color(0xFF2E8BF1),
+                ),
+                body: Center(
+                    child: Text('Continentes: ${appdata.data.length}'),
+                ),
+            )
         );
     }
 }
